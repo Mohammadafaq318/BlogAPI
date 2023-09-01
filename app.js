6b55c95401f18e17eeb91d46f08eda9e5f381755
afaq <mafaqq318@gmail.com>
2023-09-01 17:29:36 +0300

    Added MongoDB and finalised application

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const homeStartingContent = "Welcome to My Daily Journal";
+const aboutContent = "This is my Daily Journal website powered by MongoDB";
+const contactContent = "Why do you event want to contact me. Hey!, this is my secret";
 
 const app = express();
 
@@ -16,12 +18,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+
+mongoose.connect('mongodb://127.0.0.1:27017/JournalDB');
+
+const postSchema={
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model('Post', postSchema);
+posts=[]
+
 
 app.get("/", function(req, res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+
+  Post.find().then((data) => {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: data
+      });
     });
 });
 
@@ -38,32 +53,40 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-
-  posts.push(post);
-
+  });
+  post.save();
   res.redirect("/");
 
 });
 
-app.get("/posts/:postName", function(req, res){
+app.get("/posts/:postId", async function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
+  const requestPostId = req.params.postId;
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
+  const post = await Post.findById(requestPostId);
 
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+  res.render("post", {
+    title: post.title,
+    content: post.content
   });
 
+  
 });
+
+app.post('/delete-post/:postId', async (req, res) => {
+  const postId = req.params.postId;
+
+  // Find the post by its ID and delete it from the MongoDB collection
+  await Post.findByIdAndRemove(postId);
+
+  res.redirect("/");
+
+  
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
